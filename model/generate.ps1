f100a77459f89687fea5c407f132442c2738c3fb
Lukiya <lukiya@outlook.com>
2020-12-30 19:46:23 -0800

    update

--- model/generate.ps1
+++ model/generate.ps1
@@ -1,1 +1,3 @@
-protoc -I . --go_out=plugins=grpc,paths=source_relative:. *.proto
+protoc -I . --go_opt=paths=source_relative --go_out=.  *.proto
+protoc -I . --go-grpc_opt=paths=source_relative --go-grpc_out=.  *.proto
+protoc-go-tags --dir=./
