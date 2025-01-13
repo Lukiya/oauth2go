8ec1ddb6e764e6558acddf5b735a23f6bfa1c20b
Lukiya Chen <lukiya@outlook.com>
2025-01-13 11:51:49 -0800

    u

--- publish.ps1
+++ publish.ps1
@@ -1,4 +1,4 @@
 
 git push
-git tag "v1.14.0"
+git tag "v1.14.1"
 git push --tags
--- security/IPkceValidator.go
+++ security/IPkceValidator.go
@@ -27,11 +27,17 @@ func (x *DefaultPkceValidator) Verify(codeVerifier, codeChallenge, codeChallenge
 	// check code_challenge_method
 	if codeChallengeMethod == core.Pkce_Plain {
 		r = codeVerifier == codeChallenge
+		if !r {
+			slog.Debugf("code_challenge is not equal to code_verifier: %s != %s", codeChallenge, codeVerifier)
+		}
 	} else if codeChallengeMethod == core.Pkce_S256 {
 		r = codeChallenge == core.ToSHA256Base64URL(codeVerifier)
+		if !r {
+			slog.Debugf("code_challenge is not equal to SHA256(code_verifier): %s != %s", codeChallenge, core.ToSHA256Base64URL(codeVerifier))
+		}
+	} else {
+		slog.Warnf("Unsupported code_challenge_method: %s", codeChallengeMethod)
 	}
 
-	// not suppor other methods
-	slog.Warnf("Unsupported code_challenge_method: %s", codeChallengeMethod)
 	return r
 }
